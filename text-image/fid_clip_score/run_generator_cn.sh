c2b03abc564fc5933f08de3e332051377b7c7714
weifeng.genius <weifeng.genius@bytedance.com>
2023-01-14 03:14:49 +0800

    updpate

---
+++ text-image/fid_clip_score/run_generator_cn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ps aux | grep -E 'run_watch.sh|watch.py' |awk '{print $2}' | xargs kill -9 # kill previous watchdog
+guidance_scales=(1.5 2.0 3.0 4.0 5.0 6.0 7.0 8.0)
+for i in {0..7}
+do
+    echo ${i}
+    CUDA_VISIBLE_DEVICES=${i} nohup python coco_sample_generator.py --model_path ../pretrained_models/stable_cn --coco_cache_file ../dataset/coco/subset_cn.parquet --output_path ./output_cn --guidance_scale ${guidance_scales[${i}]} --batch_size 16 --sample_step 20 > stable_generator.log 2>&1 &
+done
+wait
+bash ~/release_watchdog.sh # start watchdog
